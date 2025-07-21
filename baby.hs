a4642cba5d17411c138c059eebee0abcc7539793
PLLN <pl.lakshminarasimhan@gmail.com>
2025-07-22 00:35:09 +0530

    understand types

--- baby.hs
+++ baby.hs
@@ -1,3 +1,5 @@
+import Control.Arrow (ArrowChoice (right))
+
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
@@ -18,15 +20,19 @@ times n xs = [n * x | x <- xs]
 
 boomBang xs limit = [if x <= limit then "BOOM!" else "BANG!" | x <- xs, odd x]
 
+combine :: [String] -> [String] -> [String]
 combine adjs nouns = [adj ++ " " ++ noun | adj <- adjs, noun <- nouns]
 
 length' xs = sum [1 | _ <- xs]
 
 -- machine trying to be smart (1)
+removeLowercase' :: String -> String
 removeLowercase' str = [ch | ch <- str, ch `notElem` ['a' .. 'z']]
 
+removeLowercase :: String -> String
 removeLowercase str = [ch | ch <- str, ch `elem` ['A' .. 'Z']]
 
+chrsWlkn :: (String, String, Int)
 chrsWlkn = ("Christopher", "Walken", 55)
 
 evenLists xxs = [[x | x <- xs, even x] | xs <- xxs]
@@ -34,4 +40,20 @@ evenLists xxs = [[x | x <- xs, even x] | xs <- xxs]
 -- machine trying to be smart (0)
 zipNumbersTill10 xs ys = [(x, y) | (x, y) <- zip xs ys, x + y > 10]
 
-zipTill xs limit = [(y, x) | (x, y) <- zip xs [1..], y <= limit]
+zipTill xs limit = [(y, x) | (x, y) <- zip xs [1 ..], y <= limit]
+
+rightTriangles = [(a, b, c) | a <- [1 .. 10], b <- [1 .. 10], c <- [1 .. 10], a ^ 2 + b ^ 2 == c ^ 2]
+
+-- even more tight
+rightTriangles' = [(a, b, c) | c <- [1 .. 10], b <- [1 .. c], a <- [1 .. b], a ^ 2 + b ^ 2 == c ^ 2]
+
+rightTrianglesWithPerimeter = [(a, b, c) | (a, b, c) <- rightTriangles', a + b + c == 24]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial x = product [1 .. x]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
