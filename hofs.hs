a9ac79cc66c783a7056f46c18251996befe735ba
PLLN <pl.lakshminarasimhan@gmail.com>
2025-08-04 03:42:54 +0530

    higher order functions - map and filter

--- hofs.hs
+++ hofs.hs
@@ -20,6 +20,10 @@ compareWithHundred = compare 100
 -- :t compare is (Ord a) => a -> a -> Ordering
 -- :t compare 100 is (Ord a) => (a -> Ordering)
 
+-- infix functions can be partially applied by using sections.
+-- to section an infix function, simply surround it with parentheses
+-- and only supply a parameter on one side
+
 divideBy10 :: (Floating a) => a -> a
 divideBy10 = (/ 10)
 
@@ -30,3 +34,32 @@ isUpperCase :: Char -> Bool
 isUpperCase = (`elem` ['A' .. 'Z'])
 
 -- all the above are partially applied functions that return function with left out parameters
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
+
+-- when we call flip' f without parameters x and y, it will return an f that takes two parameters
+-- but calls them flipped
+
+myFlip :: (a -> b -> c) -> (b -> a -> c)
+myFlip f = g
+  where
+    g x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x : xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x : xs)
+  | p x = x : filter' p xs
+  | otherwise = filter' p xs
