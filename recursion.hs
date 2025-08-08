e002dc5499fba30155742d8f1a724dae006a249f
PLLN <pl.lakshminarasimhan@gmail.com>
2025-08-09 01:49:42 +0530

    recursion basics

--- recursion.hs
+++ recursion.hs
@@ -12,9 +12,17 @@ maximum' (x : xs)
 myMaximum :: (Ord a) => [a] -> a
 myMaximum [] = error "it is an empty list"
 myMaximum [x] = x
-myMaximum (x:xs) = max x (myMaximum xs)
+myMaximum (x : xs) = max x (myMaximum xs)
 
 replicate' :: Int -> a -> [a]
 replicate' 0 x = []
 replicate' 1 x = [x]
-replicate' n x = x : replicate' (n - 1) x
+replicate' n x
+  | n < 0 = error "invalid input value"
+  | otherwise = x : replicate' (n - 1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x : xs) = x : take' (n - 1) xs
