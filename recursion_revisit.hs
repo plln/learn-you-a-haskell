6ec72ce2fc66530c99a00a243687c17535243446
PLLN <pl.lakshminarasimhan@gmail.com>
2026-01-04 19:44:27 +0530

    recursion - quick sort

--- recursion_revisit.hs
+++ recursion_revisit.hs
@@ -45,3 +45,10 @@ zip' :: [a] -> [b] -> [(a, b)]
 zip' [] _ = []
 zip' _ [] = []
 zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+   in smallerSorted ++ [x] ++ biggerSorted
